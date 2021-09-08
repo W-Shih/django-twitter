fb5cdaf06bdf1c3ba9b4cc3436106b49f491e53f
wshih <wen.shih@alumni.stonybrook.edu>
2021-09-07 22:16:44 -0700

    Update tests to react to TestCase refactor

--- testing/testcases.py
+++ testing/testcases.py
@@ -7,7 +7,8 @@
 #
 # =================================================================================================
 #    Date      Name                    Description of Change
-# 06-Sep-2021  Wayne Shih              Initial create
+# 06-Sep-2021  Wayne Shih              Initial create: Refactor TestCase
+# 07-Sep-2021  Wayne Shih              Correct comments
 # $HISTORY$
 # =================================================================================================
 
@@ -24,15 +25,16 @@ class TestCase(DjangoTestCase):
             password = 'default_generic_password'
         if email is None:
             email = f'{username}@django-twitter.com'
+
+        # <Wayne Shih> 12-Aug-2021
+        # Don't use User.objects.create() to create users
+        # because password needs to be hashed, also username and email need to be normalized
+        # (e.g. trim).
         return User.objects.create_user(username, email, password)
 
     def create_tweet(self, user, content=None):
         if content is None:
             content = 'Default content -- Welcome to Django-Twitter'
 
-        # <Wayne Shih> 12-Aug-2021
-        # Don't use User.objects.create() to create users
-        # because password needs to be hashed, also username and email need to be normalized
-        # (e.g. trim).
         return Tweet.objects.create(user=user, content=content)
 
--- tweets/test.py
+++ tweets/test.py
@@ -7,12 +7,12 @@
 # =================================================================================================
 #    Date      Name                    Description of Change
 # 05-Sep-2021  Wayne Shih              Initial create
+# 07-Sep-2021  Wayne Shih              React to refactoring TestCase
 # $HISTORY$
 # =================================================================================================
 
 from datetime import date, timedelta
-from django.contrib.auth.models import User
-from django.test import TestCase
+from testing.testcases import TestCase
 from tweets.models import Tweet
 from utils.time_helpers import utc_now
 
@@ -20,8 +20,8 @@ from utils.time_helpers import utc_now
 class TweetTest(TestCase):
 
     def test_hours_to_now(self):
-        kd = User.objects.create_user(username='kd')
-        tweet = Tweet.objects.create(user=kd, content='kd is MVP!')
+        user = self.create_user(username='kd')
+        tweet = self.create_tweet(user=user, content='kd is MVP!')
         tweet.created_at = utc_now() - timedelta(hours=10)
         tweet.save()
         self.assertEqual(tweet.hours_to_now, 10)
@@ -34,14 +34,14 @@ class TweetTest(TestCase):
         self.assertEqual(hasattr(Tweet, 'created_at'), True)
 
     def test_tweet_model(self):
-        user = User.objects.create_user(username='curry', password='sc30')
-        tweet = Tweet.objects.create(content='logo shot')
+        user = self.create_user(username='curry', password='sc30')
+        tweet = self.create_tweet(user=None, content='logo shot')
 
         self.assertEqual(tweet.user, None)
         self.assertEqual(tweet.created_at.day, date.today().day)
 
         tweet.delete()
-        Tweet.objects.create(user=user, content='logo shot for 333333333!')
+        self.create_tweet(user=user, content='logo shot for 333333333!')
 
         user.delete()
 
@@ -50,15 +50,15 @@ class TweetTest(TestCase):
         self.assertEqual(Tweet.content.field.max_length, 255)
 
     def test_auto_now_add(self):
-        tweet = Tweet.objects.create(content='original tweet')
+        tweet = self.create_tweet(user=None, content='original tweet')
         old_created_time = tweet.created_at
         tweet.content = 'updated tweet'
         tweet.save()
         self.assertEqual(old_created_time, tweet.created_at)
 
     def test_str(self):
-        user = User.objects.create_user(username='lbj23', password='KingJames')
-        tweet = Tweet.objects.create(user=user, content='I am the King')
+        user = self.create_user(username='lbj23', password='KingJames')
+        tweet = self.create_tweet(user=user, content='I am the King')
         # print(tweet)
         self.assertEqual(str(tweet.created_at) in str(tweet), True)
         self.assertEqual(tweet.user.username in str(tweet), True)
