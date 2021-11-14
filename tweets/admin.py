acb6ab6098c9e36292b7b94f4e2a62b339c750ee
wshih <wen.shih@alumni.stonybrook.edu>
2021-11-13 17:27:22 -0800

    feature: Add id to admin page

--- tweets/admin.py
+++ tweets/admin.py
@@ -8,6 +8,7 @@
 #    Date      Name                    Description of Change
 # 05-Sep-2021  Wayne Shih              Initial create
 # 10-Oct-2021  Wayne Shih              React to pylint checks
+# 13-Nov-2021  Wayne Shih              Add id
 # $HISTORY$
 # =================================================================================================
 
@@ -21,6 +22,7 @@ from tweets.models import Tweet
 class TweetAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     list_display = (
+        'id',
         'created_at',
         'user',
         'content',
