4374f0865a5ab804762a2cf6d3183807425d340f
wshih <wen.shih@alumni.stonybrook.edu>
2022-03-23 23:40:19 -0700

    settings: set file storage and amazon s3
    style: fix pylint
    
    doc: fix typo

--- twitter/local_settings.example.py
+++ twitter/local_settings.example.py
@@ -1,4 +1,23 @@
-# checkout https://www.neilwithdata.com/django-sql-logging
+# =================================================================================================
+#                                  All Rights Reserved.
+# =================================================================================================
+# File description:
+#       local settings for local dev env to override settings.py
+#       This is an example file, modify it as need.
+# Note:
+#       local_setting.py is used for local dev env ONLY.
+#       Do NOT push local_setting.py to github repo.
+#
+# =================================================================================================
+#    Date      Name                    Description of Change
+# 06-Nov-2021  Wayne Shih              Initial settings
+# 23-Mar-2022  Wayne Shih              Add AWS-related env variables
+# $HISTORY$
+# =================================================================================================
+
+# <Wayne Shih> 06-Nov-2021
+# Dev logging
+# https://www.neilwithdata.com/django-sql-logging
 LOGGING = {
     'version': 1,
     'filters': {
@@ -20,3 +39,17 @@ LOGGING = {
         }
     }
 }
+
+# <Wayne Shih> 20-Mar-2022
+# AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY are confidential.
+# Do NOT add these information in settings.py
+# The other option is to set them as environment variables instead.
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
+#
+# AWS_ACCESS_KEY_ID = 'YOUR_ACCESS_KEY_ID'
+# AWS_SECRET_ACCESS_KEY = 'YOUR_SECRET_ACCESS_KEY'
+#
+# The other option is to set them as environment variables instead.
+# This way it can be written in settings.py
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
