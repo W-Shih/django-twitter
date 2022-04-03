bcf90128dd4a97c6a32448d535b42b0131a0f85b
wshih <wen.shih@alumni.stonybrook.edu>
2022-04-03 11:28:02 -0700

    feature-and-test: add has_followed on friendships page with tests

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -16,6 +16,7 @@
 # 08-Sep-2021  Wayne Shih              Initial create
 # 10-Oct-2021  Wayne Shih              React to pylint checks
 # 27-Feb-2022  Wayne Shih              Add DefaultFriendshipSerializer
+# 03-Apr-2022  Wayne Shih              Add has_followed to Follower and Following Serializers
 # $HISTORY$
 # =================================================================================================
 
@@ -24,6 +25,7 @@ from rest_framework import serializers
 
 from accounts.api.serializers import UserSerializerForFriendship
 from friendships.models import Friendship
+from friendships.services import FriendshipService
 
 
 class DefaultFriendshipSerializer(serializers.Serializer):
@@ -33,21 +35,47 @@ class DefaultFriendshipSerializer(serializers.Serializer):
 class FollowerSerializer(serializers.ModelSerializer):
     from_user = UserSerializerForFriendship()
     # user = UserSerializerForFriendship(source='from_user')
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('from_user', 'created_at',)
+        fields = ('from_user', 'created_at', 'has_followed')
         # fields = ('user', 'created_at',)
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        # <Wayne Shih> 03-Apr-2022
+        # TODO:
+        #   Here causes N + 1 query problem, need to improve this by cache
+        return FriendshipService.get_has_followed(
+            from_user=self.context['request'].user,
+            to_user=obj.from_user,
+        )
 
 
 class FollowingSerializer(serializers.ModelSerializer):
     # to_user = UserSerializerForFriendship()
     user = UserSerializerForFriendship(source='to_user')
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
         # fields = ('to_user', 'created_at',)
-        fields = ('user', 'created_at',)
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        # <Wayne Shih> 03-Apr-2022
+        # TODO:
+        #   Here causes N + 1 query problem, need to improve this by cache
+        return FriendshipService.get_has_followed(
+            from_user=self.context['request'].user,
+            to_user=obj.to_user,
+        )
 
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
