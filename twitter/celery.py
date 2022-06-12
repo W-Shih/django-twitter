7b3ac70a34f08bb52eeadc95c8e179df7196abe6
wshih <wen.shih@alumni.stonybrook.edu>
2022-06-12 01:28:05 -0700

    init: install and init celery with django

--- twitter/celery.py
+++ twitter/celery.py
@@ -2,7 +2,7 @@
 #                                  All Rights Reserved.
 # =================================================================================================
 # File description:
-#     Initial celery
+#     Set up celery
 #       - https://docs.celeryq.dev/en/stable/django/first-steps-with-django.html
 #
 # =================================================================================================
@@ -12,8 +12,25 @@
 # =================================================================================================
 
 
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+import os
 
-__all__ = ('celery_app',)
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitter.settings')
+
+app = Celery('proj')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
